f6283c3ffab0633297fe431235f2cdbe0dc71095
lmbcosta <lmbcosta@hotmail.com>
2019-05-08 20:13:02 +0100

    Initial Commit

---
+++ VoipApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoipApp
+//
+//  Created by Luis  Costa on 08/05/2019.
+//  Copyright © 2019 Luis  Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
