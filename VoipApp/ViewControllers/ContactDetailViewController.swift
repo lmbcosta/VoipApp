cfe4106671c89365f51eec437095f17894d03440
lmbcosta <lmbcosta@hotmail.com>
2019-05-12 19:48:07 +0100

    Begin creating at remaining View Controllers

---
+++ VoipApp/ViewControllers/ContactDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactDetailViewController.swift
+//  VoipApp
+//
+//  Created by Luis  Costa on 12/05/2019.
+//  Copyright © 2019 Luis  Costa. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
